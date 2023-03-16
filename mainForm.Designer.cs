c05921c48eb8a6f9d097c588107771b1d18876b2
minihazel <decyanidez@gmail.com>
2023-03-16 13:41:07 +0100

    Add project files.

---
+++ mainForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace UpgradedTimers
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupLocations = new System.Windows.Forms.GroupBox();
+            this.locationsPlaceholder = new System.Windows.Forms.Label();
+            this.groupActions = new System.Windows.Forms.GroupBox();
+            this.groupExtractTimes = new System.Windows.Forms.GroupBox();
+            this.extractPlaceholder = new System.Windows.Forms.Label();
+            this.groupLocations.SuspendLayout();
+            this.groupActions.SuspendLayout();
+            this.groupExtractTimes.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupLocations
+            // 
+            this.groupLocations.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupLocations.Controls.Add(this.locationsPlaceholder);
+            this.groupLocations.ForeColor = System.Drawing.Color.LightGray;
+            this.groupLocations.Location = new System.Drawing.Point(21, 21);
+            this.groupLocations.Name = "groupLocations";
+            this.groupLocations.Size = new System.Drawing.Size(259, 372);
+            this.groupLocations.TabIndex = 0;
+            this.groupLocations.TabStop = false;
+            this.groupLocations.Text = " Locations ";
+            // 
+            // locationsPlaceholder
+            // 
+            this.locationsPlaceholder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(50)))));
+            this.locationsPlaceholder.Location = new System.Drawing.Point(1, 40);
+            this.locationsPlaceholder.Name = "locationsPlaceholder";
+            this.locationsPlaceholder.Size = new System.Drawing.Size(257, 29);
+            this.locationsPlaceholder.TabIndex = 0;
+            this.locationsPlaceholder.Text = "Placeholder";
+            this.locationsPlaceholder.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.locationsPlaceholder.Visible = false;
+            // 
+            // groupActions
+            // 
+            this.groupActions.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupActions.Controls.Add(this.groupExtractTimes);
+            this.groupActions.ForeColor = System.Drawing.Color.LightGray;
+            this.groupActions.Location = new System.Drawing.Point(295, 21);
+            this.groupActions.Name = "groupActions";
+            this.groupActions.Size = new System.Drawing.Size(313, 372);
+            this.groupActions.TabIndex = 1;
+            this.groupActions.TabStop = false;
+            this.groupActions.Text = " Actions ";
+            // 
+            // groupExtractTimes
+            // 
+            this.groupExtractTimes.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupExtractTimes.Controls.Add(this.extractPlaceholder);
+            this.groupExtractTimes.ForeColor = System.Drawing.Color.LightGray;
+            this.groupExtractTimes.Location = new System.Drawing.Point(1, 24);
+            this.groupExtractTimes.Name = "groupExtractTimes";
+            this.groupExtractTimes.Size = new System.Drawing.Size(311, 180);
+            this.groupExtractTimes.TabIndex = 2;
+            this.groupExtractTimes.TabStop = false;
+            this.groupExtractTimes.Text = " Extract times ";
+            // 
+            // extractPlaceholder
+            // 
+            this.extractPlaceholder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(50)))));
+            this.extractPlaceholder.Location = new System.Drawing.Point(1, 21);
+            this.extractPlaceholder.Name = "extractPlaceholder";
+            this.extractPlaceholder.Size = new System.Drawing.Size(309, 29);
+            this.extractPlaceholder.TabIndex = 1;
+            this.extractPlaceholder.Text = "Placeholder";
+            this.extractPlaceholder.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.extractPlaceholder.Visible = false;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(631, 417);
+            this.Controls.Add(this.groupActions);
+            this.Controls.Add(this.groupLocations);
+            this.Font = new System.Drawing.Font("Bahnschrift Light", 11F);
+            this.ForeColor = System.Drawing.Color.LightGray;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(647, 456);
+            this.Name = "mainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UpgradedTimers";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.groupLocations.ResumeLayout(false);
+            this.groupActions.ResumeLayout(false);
+            this.groupExtractTimes.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupLocations;
+        private System.Windows.Forms.Label locationsPlaceholder;
+        private System.Windows.Forms.GroupBox groupActions;
+        private System.Windows.Forms.GroupBox groupExtractTimes;
+        private System.Windows.Forms.Label extractPlaceholder;
+    }
+}
+
---
+++ mainForm.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UpgradedTimers
+{
+    public partial class mainForm : Form
+    {
+        string currentDir = Environment.CurrentDirectory;
+        public string accessFile;
+
+        public Color idleColor = Color.FromArgb(255, 35, 35, 40);
+        public Color hoverColor = Color.FromArgb(255, 40, 40, 45);
+        public Color selectColor = Color.FromArgb(255, 45, 45, 50);
+
+        public string[] locations =
+        {
+            "Customs",
+            "Factory Day",
+            "Factory Night",
+            "Interchange",
+            "Labs",
+            "Lighthouse",
+            "Reserve",
+            "Shoreline",
+            "Streets",
+            "Woods"
+        };
+
+        public string[] exfilTimes =
+        {
+            "Default",
+            "60 minutes",
+            "90 minutes",
+            "120 minutes",
+            "240 minutes"
+        };
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            accessFile = Path.Combine(currentDir, "config\\config.json");
+            bool exists = File.Exists(accessFile);
+
+            if (exists)
+            {
+                clearUI();
+                listLocations();
+            }
+        }
+
+        private void clearUI()
+        {
+            // server box
+            for (int i = groupLocations.Controls.Count - 1; i >= 0; i--)
+            {
+                Label selected = groupLocations.Controls[i] as Label;
+                if (selected != null)
+                {
+                    try
+                    {
+                        groupLocations.Controls.RemoveAt(i);
+                        selected.Dispose();
+                    }
+                    catch (Exception err)
+                    {
+                        Debug.WriteLine($"ERROR: {err.Message.ToString()}");
+                        MessageBox.Show($"Oops! It seems like we received an error. If you're uncertain what it\'s about, please message the developer with a screenshot:\n\n{err.Message.ToString()}", this.Text, MessageBoxButtons.OK);
+                    }
+                }
+            }
+        }
+
+        public void listLocations()
+        {
+            for (int i = 0; i < locations.Length; i++)
+            {
+                Label lbl = new Label();
+                lbl.Text = locations[i];
+                lbl.AutoSize = false;
+                lbl.Anchor = (AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right);
+                lbl.TextAlign = ContentAlignment.MiddleLeft;
+                lbl.Size = new Size(locationsPlaceholder.Size.Width, locationsPlaceholder.Size.Height);
+                lbl.Location = new Point(locationsPlaceholder.Location.X, locationsPlaceholder.Location.Y + (i*30));
+                lbl.Font = new Font("Bahnschrift Light", 9, FontStyle.Regular);
+                lbl.BackColor = idleColor;
+                lbl.ForeColor = Color.LightGray;
+                lbl.Margin = new Padding(1, 1, 1, 1);
+                lbl.Cursor = Cursors.Hand;
+                lbl.MouseEnter += new EventHandler(lbl_MouseEnter);
+                lbl.MouseLeave += new EventHandler(lbl_MouseLeave);
+                lbl.MouseDown += new MouseEventHandler(lbl_MouseDown);
+                lbl.MouseUp += new MouseEventHandler(lbl_MouseUp);
+                lbl.TextAlign = ContentAlignment.MiddleCenter;
+                groupLocations.Controls.Add(lbl);
+            }
+        }
+
+        private void lbl_MouseEnter(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label label = (System.Windows.Forms.Label)sender;
+            if (label.Text != "")
+            {
+                if (label.BackColor != selectColor)
+                {
+                    label.BackColor = hoverColor;
+                }
+            }
+        }
+
+        private void lbl_MouseLeave(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label label = (System.Windows.Forms.Label)sender;
+            if (label.Text != "")
+            {
+                if (label.BackColor != selectColor)
+                {
+                    label.BackColor = idleColor;
+                }
+            }
+        }
+
+        private void lbl_MouseDown(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label lbl = (System.Windows.Forms.Label)sender;
+
+            if (lbl.Text != "")
+            {
+                foreach (Control component in groupLocations.Controls)
+                {
+                    if (component.Text.Contains("> "))
+                    {
+                        component.Text = component.Text.Remove(0, 2);
+                    }
+
+                    if (component is Label && component.Text != lbl.Text)
+                    {
+                        component.BackColor = idleColor;
+                        component.ForeColor = Color.LightGray;  
+                    }
+                }
+                lbl.BackColor = selectColor;
+
+                groupActions.Text = $" Actions for {lbl.Text} ";
+                listExtracts();
+
+                string activeItem = lbl.Text;
+                activeItem = "> " + activeItem;
+                lbl.Text = activeItem;
+                lbl.ForeColor = Color.DodgerBlue;
+
+            }
+        }
+
+        private void lbl_MouseUp(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label label = (System.Windows.Forms.Label)sender;
+            if (label.Text != "")
+            {
+            }
+        }
+
+        public void listExtracts()
+        {
+            for (int i = 0; i < exfilTimes.Length; i++)
+            {
+                Label lbl = new Label();
+                lbl.Text = exfilTimes[i];
+                lbl.AutoSize = false;
+                lbl.Anchor = (AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right);
+                lbl.TextAlign = ContentAlignment.MiddleLeft;
+                lbl.Size = new Size(extractPlaceholder.Size.Width, extractPlaceholder.Size.Height);
+                lbl.Location = new Point(extractPlaceholder.Location.X, extractPlaceholder.Location.Y + (i * 30));
+                lbl.Font = new Font("Bahnschrift Light", 9, FontStyle.Regular);
+                lbl.BackColor = idleColor;
+                lbl.ForeColor = Color.LightGray;
+                lbl.Margin = new Padding(1, 1, 1, 1);
+                lbl.Cursor = Cursors.Hand;
+                lbl.MouseEnter += new EventHandler(extract_MouseEnter);
+                lbl.MouseLeave += new EventHandler(extract_MouseLeave);
+                lbl.MouseDown += new MouseEventHandler(extract_MouseDown);
+                lbl.MouseUp += new MouseEventHandler(extract_MouseUp);
+                lbl.TextAlign = ContentAlignment.MiddleCenter;
+                groupExtractTimes.Controls.Add(lbl);
+            }
+        }
+
+        private void extract_MouseEnter(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label label = (System.Windows.Forms.Label)sender;
+            if (label.Text != "")
+            {
+                if (label.BackColor != selectColor)
+                {
+                    label.BackColor = hoverColor;
+                }
+            }
+        }
+
+        private void extract_MouseLeave(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label label = (System.Windows.Forms.Label)sender;
+            if (label.Text != "")
+            {
+                if (label.BackColor != selectColor)
+                {
+                    label.BackColor = idleColor;
+                }
+            }
+        }
+
+        private void extract_MouseDown(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label lbl = (System.Windows.Forms.Label)sender;
+
+            if (lbl.Text != "")
+            {
+                foreach (Control component in groupLocations.Controls)
+                {
+                    if (component.Text.Contains("> "))
+                    {
+                        component.Text = component.Text.Remove(0, 2);
+                    }
+
+                    if (component is Label && component.Text != lbl.Text)
+                    {
+                        component.BackColor = idleColor;
+                        component.ForeColor = Color.LightGray;
+                    }
+                }
+                lbl.BackColor = selectColor;
+
+                groupActions.Text = $" Actions for {lbl.Text} ";
+
+
+                string activeItem = lbl.Text;
+                activeItem = "> " + activeItem;
+                lbl.Text = activeItem;
+                lbl.ForeColor = Color.DodgerBlue;
+
+            }
+        }
+
+        private void extract_MouseUp(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Label label = (System.Windows.Forms.Label)sender;
+            if (label.Text != "")
+            {
+            }
+        }
+    }
+}
